d202e71c4a81987c68577120a1a70f7e3787487d
Alisa <167567180+alisa777gul@users.noreply.github.com>
2024-09-26 18:41:22 +0200

    Initialize gh-pages

---
+++ index.js
@@ -0,0 +1,12 @@
+import{S as u,i as c}from"./assets/vendor-5ObWk2rO.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))e(r);new MutationObserver(r=>{for(const t of r)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&e(n)}).observe(document,{childList:!0,subtree:!0});function i(r){const t={};return r.integrity&&(t.integrity=r.integrity),r.referrerPolicy&&(t.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?t.credentials="include":r.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function e(r){if(r.ep)return;r.ep=!0;const t=i(r);fetch(r.href,t)}})();const d=new u(".list-img a");function m(o,s){const i=o.map(e=>`<li class="imgAdded">
+                <a href="${e.largeImageURL}"> 
+                    <img src="${e.webformatURL}" alt="${e.tags}" width="360" height="152">
+                </a>
+                <div class="descr">
+                    <p><b>Likes</b> ${e.likes}</p>
+                    <p><b>Views</b> ${e.views}</p>
+                    <p><b>Comments</b> ${e.comments}</p>
+                    <p><b>Downloads</b> ${e.downloads}</p>
+                </div>
+            </li>`).join("");s.insertAdjacentHTML("beforeend",i),d.refresh()}function f(o){const i=`https://pixabay.com/api/?${new URLSearchParams({key:"45999766-9f9a6b82db6e56573d0cf5f49",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0}).toString()}`;return fetch(i).then(e=>{if(!e.ok)throw new Error(`HTTP error! Status: ${e.status}`);return e.json()}).then(e=>e.hits.length<=0?(c.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),[]):e.hits).catch(e=>{c.error({title:"Error",message:"An error occurred while fetching the images. Please try again later.",position:"topRight"}),console.error(e)})}const h=document.querySelector(".formImg"),l=document.querySelector(".list-img"),a=document.querySelector(".loader"),g=document.getElementById("search-img");h.addEventListener("submit",o=>{o.preventDefault(),l.innerHTML="",a.classList.remove("visually-hidden");const s=g.value.trim();s!==""?f(s).then(i=>{a.classList.add("visually-hidden"),m(i,l)}).catch(i=>console.log(i)):(a.classList.add("visually-hidden"),c.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}))});
+//# sourceMappingURL=index.js.map
